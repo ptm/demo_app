21ff15142b6dbb7fec3dcae350da91eecd15cd11
Nadav Yonit <ptm@netvision.net.il>
2011-03-03 22:54:30 +0200

    Initial Commit PTM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '811f1e35ec87d6d414535600a6f7a9d19655c5ed561cec155ef8b05cd308e609f497f5fca714e9da52d62867d7ae7e8430cdcf2e28ae105edd2a81a9d2f7e397'
