43aff3ef148bc6bf3058163f3436d0fbc6ef0532
Nadav Yonit <ptm@netvision.net.il>
2011-03-21 23:24:18 +0200

    Initial Commit My Demo_app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '811f1e35ec87d6d414535600a6f7a9d19655c5ed561cec155ef8b05cd308e609f497f5fca714e9da52d62867d7ae7e8430cdcf2e28ae105edd2a81a9d2f7e397'
+DemoApp::Application.config.secret_token = '8a0aae32debc9f0cb3935af3ec9ca41eb7f4616fb069e1c6b61957125857620d1ca4b67110e36aef428b85c0e3c19fce9db93d8e32ebd8b480fdb651f3e28b00'
